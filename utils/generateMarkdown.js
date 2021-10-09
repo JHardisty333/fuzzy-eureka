2b14c655171f5029937d4e63ce3733c0079a9460
JHardisty333 <jessica1990_37@hotmail.com>
2021-10-09 15:01:36 -0600

    created readme structure

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,69 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ![badge](https://img.shields.io/badge/license-${data.license}-blueviolet)
+  <br />
+
+  ## Description
+  ${data.description}
+  
+ ------------------
+ ------------------
+
+ ## Table of Contents 🔎
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Credit](#credit)
+ - [Experience](#experience)
+ - [License](#license)
+ - [Github](#github)
+ - [Email](#email)
+
+ ------------------
+ ------------------
+
+ ## Installation
+  ${data.installation}
+  
+  ------------------
+
+ ## Usage
+  ${data.usage}
+
+  ------------------
+
+ ## Credit
+  ${data.credit}
+
+  ------------------
+
+ ## Experience
+  ${data.experience}
+
+  ------------------
+  
+ ## License
+  ${data.license}
+  <br>
+  ![badge](https://img.shields.io/badge/license-${data.license}-blueviolet) 
+  <br>
+  This application is protected by ${data.license} license.
+
+  ------------------
+
+ ## Github
+ [GitHub](https://github.com/${data.github})
+
+
+  ------------------
+  
+ ## Email
+ [Email Me](mailto:${data.email})
+
+  ------------------
 `;
 }
 
---
+++ utils/generatedReadME.md
@@ -0,0 +1,63 @@
+
+  # Dummy Stuff
+
+  ![badge](https://img.shields.io/badge/license-MIT-blueviolet)
+  <br />
+
+  ## Description
+  This is just a blerp description 
+  
+ ------------------
+ ------------------
+
+ ## Table of Contents 🔎
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Credit](#credit)
+ - [Experience](#experience)
+ - [License](#license)
+ - [Github](#github)
+ - [Email](#email)
+
+ ------------------
+ ------------------
+
+ ## Installation
+  No depedencies needed
+  
+  ------------------
+
+ ## Usage
+  You know, just install it.
+
+  ------------------
+
+ ## Credit
+  Kelley Worthington
+
+  ------------------
+
+ ## Experience
+  Mice
+
+  ------------------
+  
+ ## License
+  MIT
+  <br>
+  ![badge](https://img.shields.io/badge/license-MIT-blueviolet) 
+  <br>
+  This application is protected by MIT license.
+
+  ------------------
+
+ ## Github
+ [GitHub](https://github.com/JHardisty333)
+
+
+  ------------------
+  
+ ## Email
+ [Email Me](mailto:jessica.hardisty.333@hotmail.com)
+
+  ------------------
